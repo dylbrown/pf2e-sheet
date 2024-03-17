f520c1a5f74ea13c7edefbcd34642ffef8cb64f3
Dylan Brown <dylanmb193@gmail.com>
2024-03-17 16:39:10 -0700

    Support improperly specified spell lists

--- src/character/character.ts
+++ src/character/character.ts
@@ -221,12 +221,14 @@ export default class Character {
     promises.push(...this.spells.load(data, metaData.spells));
 
     for (const list of this.spells.lists) {
+      if (list.attack_attr.valueOf() == -1) continue;
       // Attack
       const attack_prof = this.attributes[list.attack_attr].proficiency;
       if (attack_prof != Proficiency.Untrained) list.attack += this.level;
       list.attack += attack_prof;
       list.attack += Math.floor((this.scores[list.score] - 10) / 2) ?? 0;
       // DC
+      if (list.dc_attr.valueOf() == -1) continue;
       const dc_prof = this.attributes[list.dc_attr].proficiency;
       if (dc_prof != Proficiency.Untrained) list.dc += this.level;
       list.dc += dc_prof;
